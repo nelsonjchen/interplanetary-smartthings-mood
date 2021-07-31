153449ab882dae206793e34cfd1a23f2d9ce6216
Nelson Chen <crazysim@gmail.com>
2021-07-30 22:16:11 -0700

    seeing it change

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import logging
+import sys
+import time
+from enum import Enum, auto
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
 
-# Press the green button in the gutter to run the script.
+class LogLineEvent(Enum):
+    UnmatchedLine = auto()
+    ActionPhaseStartLine = auto()
+    ActionPhaseEndLine = auto()
+
+
+def main():
+    log_file_name = r"C:\Program Files (x86)\Steam\steamapps\common\Interplanetary Enhanced " \
+                    r"Edition\Interplanetary_Data"
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = log_file_name
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
